f84cb7e33eb70ee710278d6fe60faaa50ac24bf4
nkatha23 <nkathakaburu0@gmail.com>
2024-11-26 21:23:32 +0300

    Sandile Energies

---
+++ generate_did.sh
@@ -0,0 +1,19 @@
+function generate_did() {
+  local canister=$1
+  canister_root="src/$canister"
+
+  cargo build --manifest-path="$canister_root/Cargo.toml" \
+      --target wasm32-unknown-unknown \
+      --release --package "$canister" \
+
+  candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" > "$canister_root/$canister.did"
+}
+
+CANISTERS=Sandile_backend
+
+for canister in $(echo $CANISTERS | sed "s/,/ /g")
+do
+    generate_did "$canister"
+done
+
+
